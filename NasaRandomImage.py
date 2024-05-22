4079a575d631bdaeee268461b1733ff049296741
James Ramsaran <james.j.ramsaran@gmail.com>
2024-05-22 18:51:21 -0400

    add wrapper function

---
+++ NasaRandomImage.py
@@ -0,0 +1,25 @@
+import json
+import zmq
+
+def get_random_nasa_photos(keywords, image_size, number_requested, user_id=0)
+
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.setsockopt(zmq.IMMEDIATE, True)  # make send() blocking by default (known issue)
+        socket.connect("tcp://localhost:8989")
+
+        request = {
+            'image_size': image_size,
+            'keywords': keywords,
+            'size': number_requested,
+            'user_id': user_id
+        }
+        print(f'Sending payload to NASA Image Search Microservice...')
+        socket.send_string(json.dumps(request))
+        print(f'Waiting for response...')
+        response = socket.recv()
+        print(f'Images received from NASA Image Search Microservice')
+        context.destroy()
+        response = json.loads(response)
+
+        return response['links']
